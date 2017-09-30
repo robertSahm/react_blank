0388866a9a975e8f4113646c057824b0cf8ff9a5
Robby Sahm <robertsahm@gmail.com>
2017-09-30 16:24:14 -0700

    updated images

--- src/components/home2.js
+++ src/components/home2.js
@@ -22,29 +22,86 @@ import Hom1 from '../img/underwear/hom_closet.jpg'
 import Wood1 from '../img/underwear/wood_1.jpg'
 import Undr1 from '../img/underwear/2undr_1.jpg'
 import Tani1 from '../img/underwear/tani_1.jpg'
+import Spanx1 from '../img/underwear/spanx_3.jpg'
 
-// Lifestyle
-import Undr1Lifestyle from '../img/lifestyle/Undr_1.jpg'
+  // Lifestyle
+import UndrLifestyle1 from '../img/lifestyle/Undr_1.jpg'
+import UndrLifestyle2 from '../img/lifestyle/Undr_2.jpg'
+import UndrLifestyle3 from '../img/lifestyle/Undr_3.jpg'
+
+  // Loungewear
+import DR_3 from '../img/loungewear/derek_rose_3.jpg'
+
+  // Socks
+import Falke1 from '../img/socks/falke_1.jpg'
+import Falke2 from '../img/socks/falke_2.jpg'
+import Falke3 from '../img/socks/falke_3.jpg'
+import Falke4 from '../img/socks/falke_4.jpg'
+import Falke5 from '../img/socks/falke_5.jpg'
+
+const CreateLink = (props) => {
+  const {path, to, text} = props;
+  return (
+    <Link
+      replace={(()=>to===path)()}
+      to={to}
+    >
+      <h3 className={'text-light text-wide text-center header-link'}>{text}</h3>
+      <img className={'hand-line'} src={HandLine} />
+    </Link>
+  );
+};
 
 class Home extends Component {
-
   render() {
-
     const settings = {
       showThumbs: false,
       showStatus: false,
-      dynamicHeight: true,
+      dynamicHeight: false,
     }
+
+    const { pathname } = this.props.location;
 
     return (
       <div className='content-home'>
-        <Carousel {...settings}>
-          <img src={Undr1Lifestyle} />
-          <img src={Hom1} />
-          <img src={Wood1} />
-          <img src={Hanro1} />
-          <img src={Tani1} />
-        </Carousel>
+        <div className="carousel-wrapper">
+          <Carousel {...settings}>
+            <img src={Spanx1} />
+            <img src={UndrLifestyle1} />
+            <img src={DR_3} />
+            <img src={UndrLifestyle2} />
+            <img src={UndrLifestyle3} />
+          </Carousel>
+          <CreateLink path={pathname} to='/underwear' text='UNDERWEAR' />
+        </div>
+        <div className="carousel-wrapper margins">
+          <Carousel {...settings}>
+            <img src={DR_3} />
+            <img src={UndrLifestyle1} />
+            <img src={Spanx1} />
+            <img src={UndrLifestyle2} />
+            <img src={UndrLifestyle3} />
+          </Carousel>
+          <CreateLink path={pathname} to='/loungewear' text='SLEEP AND LOUNGEWEAR' />
+        </div>
+        <div className="carousel-wrapper third">
+          <Carousel {...settings}>
+            <img src={Falke1} />
+            <img src={Falke2} />
+            <img src={Falke3} />
+            <img src={Falke5} />
+          </Carousel>
+          <CreateLink path={pathname} to='/loungewear' text='SOCKS' />
+        </div>
+        <div className="carousel-wrapper third">
+          <Carousel {...settings}>
+            <img src={Falke1} />
+            <img src={Falke2} />
+            <img src={Falke3} />
+            <img src={Falke5} />
+          </Carousel>
+          <CreateLink path={pathname} to='/loungewear' text='SOCKS' />
+        </div>
       </div>
     )
   }
