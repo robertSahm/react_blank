f081cb96b10ee61f297ebabca28b7048a949e80c
Robby Sahm <robertsahm@gmail.com>
2017-10-06 13:55:37 -0700

    underwear page added

--- src/components/underwear.js
+++ src/components/underwear.js
@@ -1,10 +1,50 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Textfit } from 'react-textfit'
+import GoogleMap from './googlemap'
+import Line from '../img/line.svg'
+import '../styles/underwear-page.css'
+import '../styles/typography.css'
+// images
+import Underwear1 from '../img/underwear-page/und-page-1.jpg'
+import Underwear2 from '../img/underwear-page/und-page-2.jpg'
+import Underwear3 from '../img/underwear-page/und-page-3.jpg'
+import Underwear4 from '../img/underwear-page/und-page-4.jpg'
+import Underwear5 from '../img/underwear-page/und-page-5.jpg'
+import Underwear6 from '../img/underwear-page/und-page-6.jpg'
+
+class Underwear extends Component {
 
 
-const Underwear = () =>
-	<div className="col-12">
-		<h3 className="text-center">Underwear</h3>
-	</div>;
+  render() {
+    return (
+      <div className={'underwear-wrap'}>
+        <div className={'images-wrap'}>
+          <div className={'img-wrap'}>
+            <img src={Underwear1} />
+          </div>
+          <div className={'img-wrap'}>
+            <img src={Underwear4} />
+          </div>
+          <div className={'img-wrap'}>
+            <img src={Underwear3} />
+          </div>
+          <div className={'img-wrap'}>
+            <img src={Underwear5} />
+          </div>
+          <div className={'img-wrap'}>
+            <img src={Underwear2} />
+          </div>
+          <div className={'img-wrap'}>
+            <img src={Underwear6} />
+          </div>
+        </div>
+        <div className={'line'}>
+          <img src={Line} />
+        </div>
+      </div>
+    )
+  }
+}
 
-export default Underwear;
+export default Underwear
+
