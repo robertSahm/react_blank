a2cbcbc46a21a269ab935057c6934e6010efda66
Robby Sahm <robertsahm@gmail.com>
2017-10-05 17:21:45 -0700

    new build

---
+++ build/js/1.a7be826ae36ae7be3212.js
@@ -0,0 +1,38 @@
+webpackJsonp([1],{
+
+/***/ 679:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRouterDom = __webpack_require__(32);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Socks = function Socks() {
+	return _react2.default.createElement(
+		"div",
+		{ className: "col-12" },
+		_react2.default.createElement(
+			"h3",
+			{ className: "text-center" },
+			"Socks"
+		)
+	);
+};
+
+exports.default = Socks;
+
+/***/ })
+
+});
+//# sourceMappingURL=1.a7be826ae36ae7be3212.js.map
