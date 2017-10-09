5a79292d2ebebb8d1235213a43fa0c8d04145c81
Robby Sahm <robertsahm@gmail.com>
2017-10-08 21:34:10 -0700

    new setup rockin'

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require ('path');
-// const nodemailer = require('nodemailer');
 
 const app = express();
-
-// server routes can go here...
-// app.get('./hello', (req, res) => res.send({ hi: 'there' }));
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(webpackMiddleware(webpack(webpackConfig)));
@@ -13,47 +9,11 @@ if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack');
   const webpackConfig = require('./webpack.config.js');
 } else {
-  app.use(express.static('build'));
+  app.use(express.static('dist'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
 }
 
-// nodemailer.createTestAccount((err, account) => {
-//   const transporter = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     auth: {
-//         user: 'z5oxreja2cmwvs4m@ethereal.email',
-//         pass: 'DWaNkbbtDzZa8zsdD7'
-//     }
-//   });
-
-//   // setup email data with unicode symbols
-//   let mailOptions = {
-//       from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-//       to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-//       subject: 'Hello ✔', // Subject line
-//       text: 'Hello world?', // plain text body
-//       html: '<b>Hello world?</b>' // html body
-//   };
-
-//   // send mail with defined transport object
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       return console.log(error);
-//     }
-//     console.log('Message sent: %s', info.messageId);
-//     // Preview only available when sending through an Ethereal account
-//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-//   });
-// }
-
 app.listen(process.env.PORT || 3050, () => console.log('Listening'));
 
-
-// Username  z5oxreja2cmwvs4m@ethereal.email (also works as a real inbound email address)
-// Password  DWaNkbbtDzZa8zsdD7
