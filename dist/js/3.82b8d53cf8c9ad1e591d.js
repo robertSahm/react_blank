9b6ea3abd79c74d91d7c6c33637a4e90482a5e57
Robby Sahm <robertsahm@gmail.com>
2017-10-10 15:06:32 -0700

    favicon working

---
+++ dist/js/3.82b8d53cf8c9ad1e591d.js
@@ -0,0 +1,1 @@
+webpackJsonp([3],{501:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(0),a=function(e){return e&&e.__esModule?e:{default:e}}(u),c=(n(25),function(){return a.default.createElement("div",{className:"col-12"},a.default.createElement("h3",{className:"text-center"},"Lounge"))});t.default=c}});
